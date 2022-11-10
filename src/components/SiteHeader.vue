dd3a369639bbe3806f9b0d9c5982b62485ce83af
SimoneLollini <lollinisimone@gmail.com>
2022-11-10 16:56:53 +0100

    end header add variables

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -12,7 +12,7 @@
 
             <nav>
                 <a href="/">CHARACTERS</a>
-                <a href="#">COMICS</a>
+                <a class="active_link" href="#">COMICS</a>
                 <a href="#">MOVIES</a>
                 <a href="#">TV</a>
                 <a href="#">GAMES</a>
@@ -21,8 +21,6 @@
                 <a href="#">FANS</a>
                 <a href="#">NEWS</a>
                 <a href="#">SHOP</a>
-
-
             </nav>
         </div>
     </header>
@@ -31,24 +29,36 @@
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
-.container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+header {
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
 
-    a {
-        margin-left: 1.1rem;
-        text-decoration: none;
-        color: $light_dark;
-        font-size: $nav_size;
-    }
-
-    .logo {
-        padding: 20px;
-
-        img {
-            width: 80px;
+        .active_link {
+            border-bottom: 3px solid $primary;
         }
+
+        nav {
+            a {
+                display: inline-block;
+                margin-left: 1.1rem;
+                text-decoration: none;
+                color: $light_dark;
+                font-size: $nav_size;
+                padding: 60px 0;
+            }
+        }
+
+
+        .logo {
+            padding: 20px;
+
+            img {
+                width: 80px;
+            }
+        }
+
     }
 
 }
