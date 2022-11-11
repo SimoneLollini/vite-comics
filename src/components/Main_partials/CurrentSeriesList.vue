86d41a11b2bdf9015b0d5c9af6f54dd2839d8ff3
SimoneLollini <lollinisimone@gmail.com>
2022-11-11 15:25:39 +0100

    add CurrrentSeriesList

---
+++ src/components/Main_partials/CurrentSeriesList.vue
@@ -0,0 +1,22 @@
+<script>
+import DComics from "../data/dc-comics.js";
+export default {
+    name: 'CurrentSeriesList',
+    components: {
+        DComics,
+    }
+}
+</script>
+
+<template>
+    <div class="row row-cols-6">
+        <div class="col" v-for="comic in DComics">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                alt="">
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,18 +1,20 @@
 <script>
 import Jumbotron from "./Main_partials/Jumbotron.vue";
+import CurrentSeriesList from "./Main_partials/CurrentSeriesList.vue";
+
 export default {
     name: 'SiteMain',
     components: {
         Jumbotron,
+        CurrentSeriesList
     }
 }
 </script>
 
 <template>
     <main>
-
         <Jumbotron />
-
+        <CurrentSeriesList />
     </main>
 </template>
 
